574ec53064e814059f0b92208adecd86699aaf40
kene Ojukwu <keneojukwu@kenes-MacBook-Pro.local>
2024-05-28 21:58:29 -0400

    added Footer

---
+++ src/sections/Footer/Footer.tsx
@@ -0,0 +1,17 @@
+import styles from './FooterStyles.module.css';
+
+const Footer = () => {
+  return (
+    <section
+      id='footer'
+      className={styles.container}
+    >
+      <p>
+        &copy; 2024 Kene Ojukwu. <br />
+        All rights reserved.
+      </p>
+    </section>
+  );
+};
+
+export default Footer;
